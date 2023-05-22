b0c899e41edf48566b0474e0f2d8d386d7bb2be5
unknownDaniel <86711135+unknownDaniel@users.noreply.github.com>
2023-05-22 16:28:32 +0200

    Add files via upload

---
+++ Hotelverwaltung.Models/Room.cs
@@ -0,0 +1,13 @@
+﻿namespace Hotelverwaltung.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+        public int NumberOfBeds { get; set; }
+        public bool HasKitchen { get; set; }
+        public bool HasBalcony { get; set; }
+        public bool HasAC { get; set; }
+        public decimal PricePerNight { get; set; }
+    }
+}
